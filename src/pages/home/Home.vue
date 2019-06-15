f57cb5c876ee133f11ff5b9de8c09aec1108c50d
wxj <1522718721@qq.com>
2019-06-15 20:39:34 +0800

    DD

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,13 @@
 <template>
 <div>
   <div>
-    <home-header></home-header>
-    <home-swiper></home-swiper>
-    <home-icons></home-icons>
-    <home-recommend></home-recommend>
-    <home-weekend></home-weekend>
+    <home-header :city="city"></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </div>
-  
 </template>
 <script>
 import HomeHeader from './components/Header'
@@ -26,12 +25,31 @@ export default {
      HomeRecommend,
      HomeWeekend,
   },
+  data (){
+    return {
+      city:'',
+      swiperList: [],
+      iconList:[],
+      recommendList:[],
+      weekendList:[]
+    }
+  },
   methods:{
     getHomeInfo(){
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      axios.get('/api/index.json')
+      .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc(res){
-      console.log(res)
+      // console.log(res)
+      res=res.data
+      if(res.ret&&res.data){
+        const data =res.data
+        this.city=data.city
+        this.swiperList = data.swiperList
+        this.iconList =data.iconList
+        this.recommendList =data.recommendList
+        this.weekendList=data.weekendList
+      }
     }
   },
   mounted(){
@@ -41,5 +59,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,22 +7,27 @@
              <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
-            城市
-            <span class="iconfont arrow-icon">&#xe600;</span>
-        </div>
+         <router-link to="/city">
+              <div class="header-right">
+                {{this.city}}
+                <span class="iconfont arrow-icon">&#xe600;</span>
+            </div>
+         </router-link>
     </div>
 </template>
 <script>
 export default {
-    name:'HomeHeader'
+    name:'HomeHeader',
+    props:{
+        city:String
+    }
 }
 </script>
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl'
     .header
         display:flex
-        line-height:.86rem
+        line-height:$headerHeight
         background:$bgColor
         color:#fff
         .header-left
@@ -45,6 +50,7 @@ export default {
             width:1.24rem  
             float:right
             text-align:center
+            color:#fff
             .arrow-icon
                 margin-left: -.04rem
                 font-size:.24rem  
