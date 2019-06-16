58fa3e1169c6106da77c670430fb0386d2e947cf
wxj <1522718721@qq.com>
2019-06-16 13:19:08 +0800

    finish

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,6 +16,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default {
   name:'Home',
   components:{
@@ -27,15 +28,19 @@ export default {
   },
   data (){
     return {
+      lastCity:'',
       swiperList: [],
       iconList:[],
       recommendList:[],
       weekendList:[]
     }
   },
+  computed:{
+    ...mapState(['city'])
+  },
   methods:{
     getHomeInfo(){
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city )
       .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc(res){
@@ -51,7 +56,15 @@ export default {
     }
   },
   mounted(){
+    this.lastCity =this.city
     this.getHomeInfo()
+  },
+  activated (){
+    // console.log('activated')
+   if(this.lastCity!==this.city ){
+     this.lastCity =this.city
+     this.getHomeInfo()
+   }
   }
 }
 </script>
--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,4 @@
-let defaultCity ='上海'
+let defaultCity ='北京'
 try{
   if( localStorage.city){
     defaultCity =localStorage.city
