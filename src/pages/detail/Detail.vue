d7c6b413c98d27ba40811b7804a9d26562b33116
wxj <1522718721@qq.com>
2019-06-16 17:15:38 +0800

    list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,19 +2,42 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+       <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name:'Detail',
   components:{
     DetailBanner,
     DetailHeader,
+    DetailList,
 
+  },
+  data (){
+    return {
+      list :[
+        {title:'成人票',
+          children:[
+            {title:'成人三管联票',
+              children:[
+                {title:'成人三管联票 - 某一连锁销售'},
+              ]
+            },
+            {title:'成人五管联票'},
+            ]
+        },
+        {title:'学生票'},
+        {title:'儿童票'},
+        {title:'特惠票'},
+        ]
+    }
   }
 }
 </script>
